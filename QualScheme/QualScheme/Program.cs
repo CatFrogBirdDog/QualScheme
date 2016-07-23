25821dc77953a64474fb3ea715792cdb06a9243a
Airtamis <jrggb@yahoo.com>
2016-07-23 16:08:28 -0500

    Textures enabled

--- QualScheme/QualScheme/Program.cs
+++ QualScheme/QualScheme/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
@@ -11,15 +12,20 @@ namespace QualScheme
 {
     class Program
     {
+        static int textID = 0;
         [STAThread]
         public static void Main()
         {
             using (var game = new GameWindow())
             {
+                GL.Enable(EnableCap.Texture2D);
+
                 game.Load += (sender, e) =>
                 {
                     // setup settings, load textures, sounds
                     game.VSync = VSyncMode.On;
+
+                    //game.KeyDown += new EventHandler<KeyboardKeyEventArgs>(Key.Down);
                 };
 
                 game.Resize += (sender, e) =>
@@ -34,27 +40,48 @@ namespace QualScheme
                     {
                         game.Exit();
                     }
+
+                    if (game.Keyboard[Key.Down])
+                    {
+                        textID = Program.loadTexture();
+                    }
                 };
 
                 game.RenderFrame += (sender, e) =>
                 {
                     // render graphics
                     GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
                     GL.MatrixMode(MatrixMode.Projection);
                     GL.LoadIdentity();
-                    GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+                    //GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
 
-                    GL.Begin(PrimitiveType.Triangles);
+                    if (textID != 0)
+                    {
+                        GL.Begin(BeginMode.Quads);
 
-                    GL.Color3(Color.MidnightBlue);
-                    GL.Vertex2(-1.0f, 1.0f);
-                    GL.Color3(Color.SpringGreen);
-                    GL.Vertex2(0.0f, -1.0f);
-                    GL.Color3(Color.Ivory);
-                    GL.Vertex2(1.0f, 1.0f);
+                        GL.BindTexture(TextureTarget.Texture2D, textID);
 
-                    GL.End();
+                        GL.TexCoord2(0.0f, 0.0f); GL.Vertex2(0.0f, 0.0f);//Top Left Corner
+                        GL.TexCoord2(0.0f, 1.0f); GL.Vertex2(0.0f, 1.0f);//Bottom Left Corner
+                        GL.TexCoord2(1.0f, 1.0f); GL.Vertex2(1.0f, 1.0f);//Bottom Right Corner
+                        GL.TexCoord2(1.0f, 0.0f); GL.Vertex2(1.0f, 0.0f);//Top Right Corner
+
+                        GL.End();
+                    }
+
+                    else
+                    {
+                        GL.Begin(PrimitiveType.Triangles);
+
+                        GL.Color3(Color.MidnightBlue);
+                        GL.Vertex2(-1.0f, 1.0f);
+                        GL.Color3(Color.SpringGreen);
+                        GL.Vertex2(0.0f, -1.0f);
+                        GL.Color3(Color.Ivory);
+                        GL.Vertex2(1.0f, 1.0f);
+
+                        GL.End();
+                    }
 
                     game.SwapBuffers();
                 };
@@ -62,6 +89,32 @@ namespace QualScheme
                 // Run the game at 60 updates per second
                 game.Run(60.0);
             }
+        }
+
+        public static int loadTexture()
+        {
+            String filename = "..//..//test.png";
+            if (String.IsNullOrEmpty(filename))
+                throw new ArgumentException(filename);
+
+            int id = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, id);
+
+            // We will not upload mipmaps, so disable mipmapping (otherwise the texture will not appear).
+            // We can use GL.GenerateMipmaps() or GL.Ext.GenerateMipmaps() to create
+            // mipmaps automatically. In that case, use TextureMinFilter.LinearMipmapLinear to enable them.
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            Bitmap bmp = new Bitmap(filename);
+            BitmapData bmp_data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bmp_data.Width, bmp_data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, bmp_data.Scan0);
+
+            bmp.UnlockBits(bmp_data);
+
+            return id;
         }
     }
 }
